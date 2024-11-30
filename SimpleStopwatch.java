58e057a06fb63c1fe379ec07a8e80335dc828c5b
Tim M <153383959+TimMsr@users.noreply.github.com>
2024-11-30 13:24:02 +0000

    Create SimpleStopwatch.java

---
+++ SimpleStopwatch.java
@@ -0,0 +1,46 @@
+import javax.swing.*;  
+
+public class SimpleStopwatch {
+    private static int elapsedTime = 0;
+
+    public static void main(String[] args) {
+        JFrame frame =new JFrame("Stopwatch");
+        int width = 400;
+        int height = 200;
+        frame.setSize(width,height);
+
+        JLabel currentTime = new JLabel("00:00:000",SwingConstants.CENTER); // explain swingconstants
+        
+        currentTime.setBounds(0, 0, width, height/2);
+        frame.add(currentTime);
+
+        JButton startButton = new JButton("Start");
+        JButton stopButton = new JButton("Stop");
+        JButton resetButton = new JButton("Reset");
+
+        JPanel buttons = new JPanel();
+        buttons.add(startButton);
+        buttons.add(stopButton);
+        buttons.add(resetButton);
+        frame.add(buttons);
+
+
+        Timer timer = new Timer(1, e -> { //You can include hint to use Actionlistener to simplify the counting part
+            elapsedTime++;
+            int minutes = elapsedTime / 60000;
+            int seconds = (elapsedTime / 1000) % 60;
+            int milliseconds = elapsedTime % 1000;
+            currentTime.setText(String.format("%02d:%02d:%03d", minutes, seconds, milliseconds)); //You can include hint to use a format specifier (dictates format of numbers)
+        });
+        startButton.addActionListener(e -> timer.start());
+        stopButton.addActionListener(e -> timer.stop());
+        resetButton.addActionListener(e -> {
+            timer.stop();
+            elapsedTime = 0;
+            currentTime.setText("00:00:000");
+        });
+
+        frame.setVisible(true);
+
+    }
+}
